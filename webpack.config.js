f9bc5e2fc36e155fedac56a08c4a010c0a1c2c10
a.nichiporovich <a.nichiporovich@gmail.com>
2020-04-12 10:58:21 +0300

    2.1 Настроил сборку проекта так, чтобы зависимости в JS собирались при помощи сборщика модулей Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: {
+    app: `./src/main.js`
+  },
+  devtool: `source-map`,
+  output: {
+    filename: `bundle.js`,
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, `public`),
+  },
+};

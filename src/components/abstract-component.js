c954cd7b4ecb4cd6e2b4153802cd858206e09b90
a.nichiporovich <a.nichiporovich@gmail.com>
2020-04-22 00:42:06 +0300

    5.2 Добавил в конструкторе проверку на абстрактный класс.

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,5 +1,9 @@
 export default class AbstractComponent {
   constructor() {
+    if (new.target === AbstractComponent) {
+      throw new Error(`Can't instantiate Abstract class, only concrete one.`);
+    }
+
     this._element = null;
   }
   getTemplate() {}

8f152c02a2d5c1ab52993f06b5895a4a24c45426
a.nichiporovich <a.nichiporovich@gmail.com>
2020-05-19 00:25:38 +0300

    7.1.4 Добавил поле Id в данны TripPoint.

--- src/data/trip-point.js
+++ src/data/trip-point.js
@@ -1,5 +1,6 @@
 export default class TripPoint {
   constructor(type, destination, offers, start, end, price, isFavorite) {
+    this.id = String(new Date() + Math.random());
     this.type = type;
     this.destination = destination;
     this.offers = offers;

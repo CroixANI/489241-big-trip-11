6efc297a0f06b281848f52c79af31772d30aaa7b
a.nichiporovich <a.nichiporovich@gmail.com>
2020-05-19 00:05:58 +0300

    7.1.3 Добавил метод для обновления конкретной точки маршрута.

--- src/models/trip.js
+++ src/models/trip.js
@@ -4,10 +4,22 @@ export default class Trip {
   }
 
   getPoints() {
-    return this._tasks;
+    return this._points;
   }
 
-  setPoints(tasks) {
-    this._tasks = Array.from(tasks);
+  setPoints(points) {
+    this._points = Array.from(points);
+  }
+
+  updatePoint(id, point) {
+    const index = this._points.indexOf((item) => item.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._points = [].concat(this._points.slice(0, index), point, this._points.slice(index + 1));
+
+    return true;
   }
 }
